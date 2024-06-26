49f667380b27918269ed281f74d86835204d4f66
Jeff Wheeler <jeff.wheeler@ucalgary.ca>
2024-06-26 12:11:59 -0600

    Work on exercise A

--- lab1exe_A.cpp
+++ lab1exe_A.cpp
@@ -2,7 +2,8 @@
  *  lab1exe_A.cpp
  *  ENSF 694 Lab 1, exercise A
  *  Created by Mahmood Moussavi
- *  Completed by: Student Name
+ *  Completed by: Jeff Wheeler
+ *	Submission date: July 3, 2024
  */
 
 #include <iostream>
@@ -26,7 +27,7 @@ int main(void)
     
     if(!cin)  // means if cin failed to read
     {
-        cout << "Invlid input. Bye...\n";
+        cout << "Invalid input. Bye...\n";
         exit(1);
     }
     
@@ -36,12 +37,29 @@ int main(void)
         cin >> velocity;
         if(!cin)
         {
-            cout << "Invlid input. Bye...";
+            cout << "Invalid input. Bye...";
             exit(1);
         }
     }
     
+	
     return 0;
 }
 
 
+void create_table(double v){
+	cout << "Angle" << /t << "t" << /t << "d";
+	cout << "(deg)" << /t << "(sec)" << /t << "(m)";
+}
+
+double Projectile_travel_time(double a, double v){
+	return 2 * v * sin(a) / G;
+}
+
+double Projectile_travel_distance(double a, double v){
+	return pow(v,2) / G * sin(2 * a);
+}
+
+double degree_to_radian(double d){
+	return d * PI / 180;
+}
